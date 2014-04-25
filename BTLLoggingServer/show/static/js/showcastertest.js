2102a1c048d05290b7eab07084a1fb8c33c6a82c
Sean.Chett <sean@grabyo.com>
2014-04-25 15:57:56 +0100

    all buttons working

--- BTLLoggingServer/show/static/js/showcastertest.js
+++ BTLLoggingServer/show/static/js/showcastertest.js
@@ -1,3 +1,4 @@
+//Cams
 var cam1 = 135965;
 var cam2 = 131411;
 var cam3 = 135967;
@@ -5,24 +6,51 @@ var cam4 = 135968;
 var cam5 = 135969;
 var cam6 = 135970;
 
+//Chats
 var chat1 = 135965;
 var chat2 = 135970;
+
+//Features
+var buyStreams = "buyStreams";
+var buyChat = "buyChat";
+var buyAlignment = "buyAlignment";
+var changeAlignment = "followTommy";
+
+var chatDisabler = "iFrameDisabler";
+
+var changeVidCam1 = "changeVidCam1";
+var changeVidCam2 = "changeVidCam2";
+var changeVidCam3 = "changeVidCam3";
+var changeVidCam4 = "changeVidCam4";
+var changeVidCam5 = "changeVidCam5";
+var changeVidCamAlignment = "changeVidCamAlignment";
+
+var changeChatAlignment = "changeChatAlignment";
 
 var userVariables = {
 	name : "",
 	credit : "",
-	chat_permission : "false",
-	streams_permission : "false",
+	chat_permission : false,
+	stream_permission : false,
+	secret_permission : false,
 	currentVid : cam1
 }
 
 initializeUserVariables();
 
 function initializeUserVariables() {
-	$.post( "http://127.0.0.1:8000/show/upgrade/", function( data ) {
+	$.get( "http://127.0.0.1:8000/show/status/", function( data ) {
 		editUserVariables(data);
 	});
 } 
+
+setInterval(function(){
+	//get json response
+	$.get( "http://127.0.0.1:8000/show/status/", function( data ) {
+		console.log("interval");
+		editUserVariables(data);
+	});
+},3000);
 
 function editUserVariables(variablesJson) {
 	/*
@@ -30,13 +58,15 @@ function editUserVariables(variablesJson) {
 	console.log(variablesJson.name);
 	console.log(variablesJson.credit);
 	console.log(variablesJson.chat_permission);
-	console.log(variablesJson.streams_permission);
+	console.log(variablesJson.stream_permission);
+	console.log(variablesJson.secret_permission);
 
 	console.log("user permission pre");
 	console.log(userVariables.name);
 	console.log(userVariables.credit);
 	console.log(userVariables.chat_permission);
-	console.log(userVariables.streams_permission);
+	console.log(userVariables.stream_permission);
+	console.log(userVariables.secret_permission);
 */
 	if (userVariables.name != variablesJson.name) {
 		userVariables.name = variablesJson.name;
@@ -46,6 +76,8 @@ function editUserVariables(variablesJson) {
 		userVariables.credit = variablesJson.credit;
 		changeFeature("x-userCredit", variablesJson.credit);
 	}
+
+	//chat
 	if (userVariables.chat_permission != variablesJson.chat_permission) {
 		userVariables.chat_permission = variablesJson.chat_permission;
 		if (variablesJson.chat_permission == true) {
@@ -56,77 +88,114 @@ function editUserVariables(variablesJson) {
 			//log error
 		}
 	}
-	if (userVariables.streams_permission != variablesJson.streams_permission) {
-		if (variablesJson.chat_permission == true) {
+
+	//streams
+	if (userVariables.stream_permission != variablesJson.stream_permission) {
+		userVariables.stream_permission = variablesJson.stream_permission;
+		if (variablesJson.stream_permission == true) {
+			enableSelectStreams();
+		} else if (variablesJson.stream_permission == false) {
+			//disableSelectStreams();
+		} else {
+			//log error
+		}
+	}
+
+	//secret
+	if (userVariables.secret_permission != variablesJson.secret_permission) {
+		userVariables.secret_permission = variablesJson.secret_permission;
+		if (variablesJson.secret_permission == true) {
 			enableAlignment();
-		} else if (variablesJson == false) {
+		} else if (variablesJson.secret_permission == false) {
 			//disableAlignment();
 		} else {
 			//log error
 		}
 	}
+
+
 /*
 	console.log("user permission post");
 	console.log(userVariables.name);
 	console.log(userVariables.credit);
 	console.log(userVariables.chat_permission);
-	console.log(userVariables.streams_permission);*/
+	console.log(userVariables.stream_permission);
+	console.log(userVariables.secret_permission);
+	*/
 }
 
-setInterval(function(){
-	//get json response
-	$.post( "http://127.0.0.1:8000/show/upgrade/", function( data ) {
-		console.log("interval");
-		editUserVariables(data);
-	});
-},3000);
 
 function spendCredit(pItem) {
 	console.log("spendCredit on " + pItem);
 	if (pItem == "alignment") {
-		$.post( "http://127.0.0.1:8000/show/upgrade/view_secret/", function( data ) {
+		$.post( "http://127.0.0.1:8000/show/upgrade/secret/", function( data ) {
 			console.log( data );
-         	data = data.split(":");
-         	$("#creditview").html("user.username credit "+data[0]);
-         	if(data[1] == "1"){
-         		enableAlignment();
-				
-			};
+         	enableAlignment();
+		});
+	} else if (pItem == "streams") {
+		$.post( "http://127.0.0.1:8000/show/upgrade/select_stream/", function( data ) {
+			console.log( data );
+         	//data = data.split(":");
+         	enableSelectStreams();
 		});
 	} else if (pItem == "chat") {
 		$.post( "http://127.0.0.1:8000/show/upgrade/chat/", function( data ) {
 			console.log( data );
-         	data = data.split(":");
-         	$("#creditview").html("user.username credit "+data[0]);
-         	if(data[1] == "1"){
-         		enableChat();
-			};
+         	//data = data.split(":");
+         	enableChat();
 		});
-	}
+	} 
+}
+
+function followTommy() {
+	enableFeature(buyAlignment);
+	disableFeature(changeAlignment);
+}
+
+function enableSelectStreams() {
+	console.log("enableSelectStreams");
+	disableFeature(buyStreams);
+	enableFeature(changeVidCam2);
+	enableFeature(changeVidCam3);
+	enableFeature(changeVidCam4);
+	enableFeature(changeVidCam5);
 }
 
 function enableAlignment() {
-	enableFeature("changeVidCamAlignment");
-	enableFeature("changeChatAlignment");
-	$("#buyAlignment").attr("disabled", "disabled");
-	$("#followTommy").attr("disabled", "disabled");
+	console.log("enableAlignment");
+	enableFeature(changeVidCamAlignment);
+	enableFeature(changeChatAlignment);
+	disableFeature(buyAlignment);
+	disableFeature(changeAlignment);
 }
 
 function enableChat() {
-	hideFeature("iFrameDisabler");
-	$("#buyChat").attr("disabled", "disabled");
+	console.log("enableChat");
+	hideFeature(chatDisabler);
+	disableFeature(buyChat);
+}
+
+function disableSelectStreams() {
+	console.log("disableSelectStreams");
+	disableFeature(changeVidCam2);
+	disableFeature(changeVidCam3);
+	disableFeature(changeVidCam4);
+	disableFeature(changeVidCam5);
+	enableFeature(buyStreams);
 }
 
 function disableAlignment() {
-	disableFeature("changeVidCamAlignment");
-	disableFeature("changeChatAlignment");
-	$("#buyAlignment").removeAttr("disabled");
-	$("#followTommy").removeAttr("disabled");
+	console.log("disableAlignment");
+	disableFeature(changeVidCamAlignment);
+	disableFeature(changeChatAlignment);
+	enableFeature(buyAlignment);
+	enableFeature(changeAlignment);
 }
 
 function disableChat() {
+	console.log("disableChat");
 	showFeature("iFrameDisabler");
-	$("#buyChat").removeAttr("disabled");
+	enableFeature("buyChat");
 }
 
 function changeFeature(feature, variable) {
@@ -135,27 +204,27 @@ function changeFeature(feature, variable) {
 }
 
 function disableFeature(feature) {
-	console.log("disable" + feature);
+	console.log("disable " + feature);
 	$("#" + feature).attr("disabled","disabled");
 }
 
 function enableFeature(feature) {
-	console.log("enable" + feature);
+	console.log("enable " + feature);
 	$("#" + feature).removeAttr("disabled");
 }
 
 function hideFeature(feature) {
-	console.log("hide" + feature);
+	console.log("hide " + feature);
 	$("#" + feature).hide();
 }
 
 function showFeature(feature) {
-	console.log("show" + feature);
+	console.log("show " + feature);
 	$("#" + feature).show();
 }
 
 function swapVideo(videoId) {
-	console.log("swap video to" + videoId);
+	console.log("swap video to " + videoId);
 	var regEx = /\d{6}/;
 	$("#videoSwapper").attr('src', $("#videoSwapper").attr('src').replace(regEx, videoId));
 }
