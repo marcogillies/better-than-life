b992e31908668a8191711004c7d7f0c6bb282d89
Marco Gillies <m.gillies@gold.ac.uk>
2014-04-25 14:48:04 +0100

    added the ability to set specific permissions via the upgrade url

--- BTLLoggingServer/show/urls.py
+++ BTLLoggingServer/show/urls.py
@@ -5,7 +5,7 @@ from show import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.user_login, name='login'),
-    url(r'^upgrade/$', views.user_upgrade, name='upgrade'),
+    url(r'^upgrade/(?P<type>[a-zA-Z0-9_]+)/$', views.user_upgrade, name='upgrade'),
     url(r'^status/$', views.user_status, name='status'),
     url(r'^mouseMove/(?P<xpos>\d\.\d+)/(?P<ypos>\d\.\d+)/?$', views.mouseMove, name='mousemove'),
     url(r'^averageMouse/$', views.averageMouse, name='averageMouse'),
